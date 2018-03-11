a63d097a8969f4c64abb082b3c95eda09229f4a9
Hazumi <shirohazumi@openmailbox.org>
2018-03-11 16:48:07 -0400

    started shop

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
 
+  const $level = $('#level');
   const $hp = $('#hp');
   const $maxHp = $('#max-hp');
   const $mp = $('#mp');
   const $maxMp = $('#max-mp');
   const $xp = $('#xp');
   const $xpToLvl = $('#xp-to-lvl');
+  const $gold = $('#gold');
   const $atkBtn = $('#attack');
   const $fleeBtn = $('#flee');
   const $townPanel = $('#town-panel');
   const $attackPanel = $('#attack-panel');
+  const $shopPanel = $('#shop-panel');
   const $monsterPanel = $('.right');
   const $monsterName = $('#monster-name');
   const $monsterAvatar = $('#monster-avatar');
@@ -18,6 +21,7 @@ $(document).ready(function() {
   const $log = $('#log');
 
   const $exWastelands = $('#ex-wastelands');
+  const $shop = $('#shop');
 
   const game = {
     player: {
@@ -27,8 +31,9 @@ $(document).ready(function() {
       mp: 10,
       maxMp: 10,
       xp: 0,
-      xpToLvl: 3,
+      xpToLvl: 8,
       atk: 5,
+      gold: 0,
       inventory: []
     },
     state: {
@@ -38,11 +43,13 @@ $(document).ready(function() {
       Bloodbeast: function() {
         this.name = 'Bloodbeast';
         this.avatar = 'http://via.placeholder.com/120x120';
-        // this.hp = 45;
-        this.hp = 4;
-        this.atk = 6;
-        this.atkRng = 1;
-        this.xp = 1;
+        this.hp = 45;
+        // this.hp = 4;
+        this.atk = 5;
+        this.atkRng = 2;
+        this.xp = 2;
+        this.gold = 2;
+        this.goldRng = 2;
       },
       attack: function(enemy) {
         let getAtkNum = getRandomInt(enemy.atk - enemy.atkRng, enemy.atk + enemy.atkRng)
@@ -50,29 +57,43 @@ $(document).ready(function() {
         logText(`${enemy.name} hit you for ${getAtkNum} damage.`);
         updatePlayerStats();
       }
-    }
+    }/*,
+    items: {
+      hpPotion: {
+        name: 'HP Potion'
+      }
+    }*/
   };
 
-  function getRandomInt(min, max) {
+  const getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  function nextLevel(level){
-    let exponent = 1.5;
-    let baseXP = 10;
+  const nextLevel = level => {
+    let exponent = 1.4;
+    let baseXP = 8;
     return Math.floor(baseXP * (level ** exponent));
   }
 
-  const updatePlayerXP = () => {
-    if (game.player.xp !== $xp.text()) {
-      if (game.player.xp >= game.player.xpToLvl) {
-        game.player.xp -= game.player.xpToLvl;
-        game.player.xpToLvl = nextLevel(game.player.level + 1);
-      } else {
-        $xp.text(game.player.xp);
-      }
+  const updatePlayerXP = (monster) => {
+    game.player.xp += monster.xp;
+    $xp.text(game.player.xp);
+    if (game.player.xp >= game.player.xpToLvl) {
+      game.player.xp -= game.player.xpToLvl;
+      $xp.text(game.player.xp);
+      let nextLvlXp = nextLevel(game.player.level + 1)
+      game.player.xpToLvl = nextLvlXp;
+      $xpToLvl.text(nextLvlXp);
+      game.player.level++;
+      $level.text(game.player.level);
     }
   };
+
+  const updatePlayerGold = (monster) => {
+    game.player.gold += monster.gold;
+    $gold.text(game.player.gold);
+  };
+
   const updatePlayerStats = () => {
     if (game.player.hp !== $hp.text()) {
       if (game.player.hp <= 0) {
@@ -93,6 +114,10 @@ $(document).ready(function() {
     }
   };
 
+  /* const refreshInventory = () => {
+
+  }; */
+
   const renderPanels = modeType => {
     if (modeType == 'combat') {
       $townPanel.hide();
@@ -100,19 +125,23 @@ $(document).ready(function() {
       $monsterPanel.show();
       game.state.combat = true;
     } else if (modeType == 'town') {
-      $monsterPanel.hide();
       $townPanel.show();
       $attackPanel.hide();
+      $monsterPanel.hide();
       game.state.combat = false;
+    } else if (modeType == 'shop') {
+      $townPanel.hide();
+      $attackPanel.hide();
+      $shopPanel.show();
     }
-  }
+  };
 
   const checkKill = monster => {
     if (monster.hp <= 0) {
       setTimeout(function() {
         logText(`You have defeated a ${monster.name}.`);
-        game.player.xp += monster.xp;
-        updatePlayerXP();
+        updatePlayerXP(monster);
+        updatePlayerGold(monster);
         renderPanels('town');
         $atkBtn.off('click');
       }, 1000);
@@ -169,6 +198,10 @@ $(document).ready(function() {
     });
   };
 
+  const shop = items => {
+    renderPanels('shop');
+  };
+
   setInterval(() => {
     if (!game.state.combat) {
       if(game.player.hp < game.player.maxHp) {
@@ -184,4 +217,9 @@ $(document).ready(function() {
   $exWastelands.on('click', () => {
     combat(new game.monster.Bloodbeast());
   });
+
+  $shop.on('click', () => {
+    shop();
+  });
+
 });
